70aaa26505671f8680411a94b6d088447b0ea35d
Brian Tavares <btavares79@gmail.com>
2023-05-26 13:53:13 -0400

    Initial Commit

---
+++ Challenge1_Unit_Converter/Challenge1_Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1_Unit_ConverterApp.swift
+//  Challenge1_Unit_Converter
+//
+//  Created by Brian Tavares on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1_Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1_Unit_Converter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Challenge1_Unit_Converter
+//
+//  Created by Brian Tavares on 5/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
